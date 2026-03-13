e9f499108c2ea57173baf207ef173f82d8d74a50
YoelRbuio2000 <yoelrubio2000@gmail.com>
2026-03-12 22:26:44 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,3 @@
+document.getElementById('saludar').addEventListener('click', function() {
+    alert('¡Hola! Soy Yoel, gracias por visitar mi primer portfolio. Espero poder aprender mas de programacion y trabajar pronto');
+});
